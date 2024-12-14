cccb02c87f1a172bcd7f557423d3f3d764d23053
unanmed <1319491857@qq.com>
2024-12-14 20:42:09 +0800

    init

---
+++ src/pages/main/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="main-div">
+        <div class="header">tMod 开发小工具</div>
+        <div class="tool-list">
+            <div
+                class="tool-one"
+                v-for="[name, url] of tools"
+                @click="open(url)"
+            >
+                <div class="tool-button">
+                    {{ name }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const tools = [
+    ['家具贴图描边', `${import.meta.env.BASE_URL}furnitureHighlights`]
+] satisfies [string, string][];
+
+function open(url: string) {
+    window.open(url);
+}
+</script>
+
+<style lang="less" scoped>
+.main-div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.header {
+    font-size: 36px;
+}
+
+.tool-one {
+    font-size: 18px;
+    padding: 8px 48px;
+    width: 300px;
+    height: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e9e9e9;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.tool-one:hover {
+    background-color: #d6d6d6;
+}
+
+.tool-list {
+    margin-top: 18px;
+    border-top: 1px solid #ddd;
+    padding-top: 18px;
+    max-height: 50%;
+    overflow: auto;
+    margin-bottom: 72px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { createPages, createMpaPlugin } from 'vite-plugin-virtual-mpa';
+
+const pages = createPages([
+    {
+        name: 'index',
+        entry: '/index.html',
+        filename: 'index.html',
+        data: {
+            title: 'tMod 开发小工具'
+        }
+    },
+    {
+        name: 'furnitureHighlights',
+        entry: '/src/pages/furnitureHighlights/main.ts',
+        filename: 'furnitureHighlights/index.html',
+        data: {
+            title: '家具贴图描边'
+        }
+    }
+]);
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue(), createMpaPlugin({ pages })],
+    base: '/tmod-dev-tools/',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                furnitureHighlights: resolve(
+                    __dirname,
+                    'src/pages/furnitureHighlights'
+                )
+            }
+        }
+    }
+});
